d864ce6cd0eae6f5d4d3bbfe3ff40d8fc8b2091f
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2021-07-26 23:23:57 +0900

    Update Level2 SQL 동명 동물 수 찾기 Oracle.sql

--- SQL/Level2 SQL 동명 동물 수 찾기 Oracle.sql
+++ SQL/Level2 SQL 동명 동물 수 찾기 Oracle.sql
@@ -1,4 +1,8 @@
 /* 링크 : https://programmers.co.kr/learn/courses/30/lessons/59041 */ 
+/* 문제 조건 : 동물 보호소에 들어온 동물 이름 중 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 SQL문을 작성해주세요. 
+이때 결과는 이름이 없는 동물은 집계에서 제외하며, 결과는 이름 순으로 조회해주세요.
+오답 이유 : 두 번 이상 조건 놓침, where 절 사용해 null 제외해야함 */
+
 
 SELECT NAME, COUNT(*) COUNT
 FROM ANIMAL_INS 
