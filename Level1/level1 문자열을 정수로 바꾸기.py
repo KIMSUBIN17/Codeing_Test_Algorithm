b0b9a4da1120b8cec173f02c5babb441ffea8bf1
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-01 18:34:59 +0900

    Create level1 문자열을 정수로 바꾸기.py

---
+++ Level1/level1 문자열을 정수로 바꾸기.py
@@ -0,0 +1,10 @@
+/*문제링크 : https://programmers.co.kr/learn/courses/30/lessons/12925 */
+
+def solution(s):
+    answer = int(s)
+    return answer
+
+
+#1.숫자 형태의 문자열 s입력받음
+#2.숫자 형태의 문자열 s를 int형으로 저장
+#3.int형으로 리턴
