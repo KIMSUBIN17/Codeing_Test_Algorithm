dde92e4eb4bc4e241095f754d360cc7d001c06ed
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-08 00:18:28 +0900

    Update Level1 K번째 수.py

--- Level1/Level1 K번째 수.py
+++ Level1/Level1 K번째 수.py
@@ -10,3 +10,11 @@ def solution(array, commands):
         answer.append(slice_arr[select-1])
     
     return answer
+
+
+'''
+list[시작 : 끝]
+- 시작은 포함, 끝은 포함X
+start의 인덱스는 + 1이 되어있기때문에 start 인덱스는 - 1을 해준다. 
+end는 -1을 하지 않고 그냥 그대로 써준다.
+'''
