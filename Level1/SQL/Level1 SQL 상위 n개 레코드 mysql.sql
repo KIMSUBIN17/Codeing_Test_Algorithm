dd96f09b41d99cf295800d9f9301b768c16090f7
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-17 16:33:42 +0900

    Update Level1 SQL 상위 n개 레코드 mysql.sql

--- Level1/SQL/Level1 SQL 상위 n개 레코드 mysql.sql
+++ Level1/SQL/Level1 SQL 상위 n개 레코드 mysql.sql
@@ -1,3 +1,10 @@
 /*링크 : https://programmers.co.kr/learn/courses/30/lessons/59405 */
 
 SELECT NAME FROM ANIMAL_INS ORDER BY DATETIME LIMIT 1;
+
+'''
+LIMIT :  상위 N개 행만 조회
+
+limit 0, 1 : 위에서 0부터 1까지의 정보 추출
+limit 1 : 위에서 1개의 정보 추출
+'''
