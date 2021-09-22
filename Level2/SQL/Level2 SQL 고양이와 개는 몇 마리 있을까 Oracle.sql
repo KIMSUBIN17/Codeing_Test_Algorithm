b1b3342a092a57da2450fe3bdbe4f552a866b9e0
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-22 21:20:21 +0900

    Update Level2 SQL 고양이와 개는 몇 마리 있을까 Oracle.sql

--- Level2/SQL/Level2 SQL 고양이와 개는 몇 마리 있을까 Oracle.sql
+++ Level2/SQL/Level2 SQL 고양이와 개는 몇 마리 있을까 Oracle.sql
@@ -1,6 +1,10 @@
 /* 링크 : https://programmers.co.kr/learn/courses/30/lessons/59040 */
 
-SELECT ANIMAL_TYPE, COUNT(ANIMAL_ID)
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_ID) AS "count"
 FROM ANIMAL_INS
 GROUP BY ANIMAL_TYPE
 ORDER BY ANIMAL_TYPE ASC;
+
+
+#검색해보면 동물의 타입은 개, 고양이2종류임
+#group by로 개와 고양이를 분리한 다음 각각 몇마리씩인지 확인
