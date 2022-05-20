eb4334a9eb8322a56666fdb80e04fdfdd58a67d7
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2022-05-20 17:07:54 +0900

    Update Level1 정수 내림차순으로 배치하기_ver2.py

--- Level1/Level1 정수 내림차순으로 배치하기_ver2.py
+++ Level1/Level1 정수 내림차순으로 배치하기_ver2.py
@@ -11,3 +11,15 @@ def solution(n):
 2. list n을 sort()함수로 내림차순 정렬(reverse=True)
 3. list->string으로 ""join()변환하고 int로 변환해 return
 '''
+
+
+
+
+'''
+- sorted() : 리스트, 문자열 둘다 적용 간으 / 정렬결과 리턴 -> 변수에 담기 가능
+- sort() : 리스트만 가능, SORT()로 문자열 정렬 시 ERROR발생
+-> 리턴X.. 입력을 출력으로 덮어쓰기 때문에 별도의 추가 공간 필요X
+ / 변수 담기 불가 --> 에러발생
+
+
+'''
