5b8e6e21f0da449e56dac87576b3edf5f32235b0
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2022-11-11 16:08:37 +0900

    Update Level1 SQL 12세 이하인 여자 환자 목록 출력하기 Oracle.sql

--- Level1/SQL/Level1 SQL 12세 이하인 여자 환자 목록 출력하기 Oracle.sql
+++ Level1/SQL/Level1 SQL 12세 이하인 여자 환자 목록 출력하기 Oracle.sql
@@ -5,3 +5,11 @@ SELECT PT_NAME, PT_NO, GEND_CD, AGE, NVL(TLNO,'NONE') AS TLNO
 FROM PATIENT 
 WHERE AGE <= 12 AND GEND_CD = 'W'
 ORDER BY AGE DESC, PT_NAME ASC
+
+
+
+/*
+여러개의 조회 조건 필요할 때는 ORDER BY 절에 두개 쓰면됨
+결과값에서 데이터가 없으면 NONE으로 표시되게 하라 했기 때문에 NVL 사용해 NULL값이면 NONE값 넣어줌
+
+*/
