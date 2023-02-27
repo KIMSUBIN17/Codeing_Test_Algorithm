43df199c8ca5e099d132ff3f557a19d19f2c9d67
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-02-27 23:49:47 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    return [x*2 for x in numbers]
+
+'''
+def solution(numbers):
+    answer = []
+    for num in numbers:
+        answer.append(num*2)
+    return answer
+'''
