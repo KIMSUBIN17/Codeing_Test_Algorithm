90b68dabfa22f5b87ba7eea46c03e718480348ac
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-03-13 22:10:10 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = ''.join(reversed(my_string))
+    return answer
+
+'''
+#다른 풀이
+def solution(my_string):
+    answer = my_string[::-1]
+    return answer
+'''
