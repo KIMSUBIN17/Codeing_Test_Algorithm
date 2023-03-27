9edb5e2d725d6d896daee715beed96275847d841
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-03-27 22:31:26 +0900

    [level 0] Title: 배열 자르기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.py
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.py
@@ -1,2 +1,8 @@
 def solution(numbers,num1,num2):
     return numbers[num1:num2+1]     #num1부터 num2+1까지
+
+'''
+인덱스 숫자로 list 슬라이스를 할 수 있음
+list[start.index:end.index] --> start index에서 end.index 바로 앞까지 슬라이싱 가능
+문제는 num2번째 인덱스까지 슬라이싱 --> end index+1이됨
+'''
