929645ed2d8131c057131d7b525c4fb0f0005d0f
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-04-12 23:35:39 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,2 +1,14 @@
 def solution(box, n):
     return (int(box[0]//n) * int(box[1]//n) * int(box[2]//n))
+
+
+'''
+#다른사람의 풀이
+import math
+
+def solution(box, n):
+    return math.prod(map(lambda v: v//n, box))
+
+- math.prod() : ()안에 있는 모든 iterable 요소들의 곱을 구함
+->box안에 있는 모든 값 v들을 n으로 나눈 값의 몫을 모두 곱하는 값을 구하는 것
+'''
