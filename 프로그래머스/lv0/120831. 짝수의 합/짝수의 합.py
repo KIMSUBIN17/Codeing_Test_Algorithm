6483babf23a0361ff68d9541c1aef5e1e6b1ef75
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-06-29 21:59:44 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
@@ -9,5 +9,12 @@ def solution(n):
 '''
 def solution(n):
     return sum([i for i in ragne(2,n+1,2)])
+    
+def solution (b):
+    s = 0       //저장공간
+    for  i in ange(n+1):
+        if i %  2 ==  0:
+            s =+ i
+    return s
 
 '''
