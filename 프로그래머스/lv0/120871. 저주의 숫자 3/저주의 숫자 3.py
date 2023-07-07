383bf4bdb9038348ab2855510508b7294eb08cf8
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-07 11:23:02 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -10,3 +10,22 @@ def solution(n):
 정수n만큼 for문 돌면서,
 3으로 나누어 떨어지거나(=3의배수) or 문자에 3이 포함되면 1을 한번 더해줌(해당숫자 건너뛰기위해)
 '''
+
+
+'''
+다른 풀이
+def solution(n):
+    answer = 0
+    for i in range(n):
+        answer += 1
+        while True:
+            if str(answer).count("3") > 0:
+                answer += 1
+            elif answer%3 == 0:
+                answer += 1
+            else:
+                break
+
+    return answer
+
+'''
