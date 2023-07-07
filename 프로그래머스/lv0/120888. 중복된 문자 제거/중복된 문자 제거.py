8391f86a578c5a27ff40c6bda394b0ec1bc874dd
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-07 15:18:17 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -4,3 +4,9 @@ def solution(my_string):
         if i not in answer:
             answer += i
     return answer
+
+'''
+# 딕셔너리 사용-리스트의 중복 제거할때 사용
+def solution(my_string):
+    return ''.join(dict.fromkeys(my_string))
+'''
