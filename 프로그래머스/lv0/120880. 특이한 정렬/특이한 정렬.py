223838e908541e4da556d62d2191fd3f9b91fb32
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-08 23:36:31 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -6,4 +6,13 @@ def solution(numlist, n):
 '''
 n과의 거리의 절대값 =  abs(x-n)에 대해 정렬하고,
 abs(n-x)가 같으면 n-x가 큰 값을 먼저 정렬
+
+
+#다른사람의 풀이
+def solution(numlist,n):
+    numlist = [(abs(n-num), -num) for num in numlist]
+    #첫번재 요소를기준으로 오름차순 정렬 and 두번째 요소 기준으로 내림차순 정렬
+    numlist.sort()
+    #두번째 요소만  리턴
+    return [-num for _, num in numlist]
 '''
