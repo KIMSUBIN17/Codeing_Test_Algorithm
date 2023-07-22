2f2732a50d9e5c25f9d2d6f79dc18f2b73c2d681
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-22 17:57:44 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -2,7 +2,17 @@ def solution(array):
     return [max(array), array.index(max(array))]
 
 '''
+인덱스 -배열의 위치값
 list에서 가장 큰수 찾기 : max()
 list에서 가장 작은 수 찾기 : min()
 list에서 특정 값의 index추출 : index()
+
+
+#다른 풀이
+def solution(array):
+    answer = [0,0]
+    answer[0] = max(array)
+    #answer.append(max(array))
+    answer[1] = answer.index(max(array))
+    return answer
 '''
