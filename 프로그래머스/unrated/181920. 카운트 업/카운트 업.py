1d311d872822355f9218951505c79dfdd2e4f0f0
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-23 22:54:14 +0900

    [unrated] Title: 카운트 업, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
@@ -1,2 +1,15 @@
 def solution(start, end):
+    answer = []
+
+    for i in range(start,end+1):
+        answer.append(i)
+    return answer
+
+
+'''
+#짧은풀이
+
+def solution(start, end):
     return [i for i in range(start,end+1)]
+    
+'''
