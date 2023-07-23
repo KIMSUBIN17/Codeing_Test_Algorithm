83d050ecd3bda27c02da7d670c80ae4e617e2527
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-23 22:06:16 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,3 +1,15 @@
 def solution(numbers):
     numbers.sort(reverse=True)
     return numbers[0]*numbers[1]
+
+'''
+# 다른 풀이
+--> 배열의 원소중에서 최댓값 뽑아서 곱하기
+def solution(numbers):  #[0,31,24,10,1,9] 정렬안된상태
+    numbers.sort()      #오름차순
+    return numbers[-1]*numbers[-2]
+    
+    
+
+
+'''
