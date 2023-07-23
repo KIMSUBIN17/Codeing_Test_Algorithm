29050fb542fe1aac7dae22ca5bd86245783c2591
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-23 21:53:30 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,3 +1,14 @@
 def solution(my_string):
     return sorted([int(i) for i in str(my_string) if i.isdigit()])
- 
+
+
+'''
+# 다른 풀이
+answer = []
+for i in my_string:
+    if i.isdigit():     #값이 숫자가 맞다면
+        answer.append(int(i))
+    answer.sort()
+    return answer
+
+'''
