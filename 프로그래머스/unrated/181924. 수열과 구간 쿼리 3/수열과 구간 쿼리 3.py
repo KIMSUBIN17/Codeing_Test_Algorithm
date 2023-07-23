b31d22296a690822ded5f4acea53f1703f654fdd
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-07-23 22:47:08 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,4 +1,14 @@
 def solution(arr, queries):
+    answer = []
+    for i in queries:
+        arr[i[0]], arr[i[1]] = arr[i[1]], arr[i[0]]
+    return arr
+
+
+
+'''
+#다른사람의 풀이
+def solution(arr, queries):
     new_arr = arr
     # 순서 바꾸기
     for q in queries:
@@ -7,3 +17,4 @@ def solution(arr, queries):
         new_arr[q[0]] = second_num
         new_arr[q[1]] = first_num
     return new_arr
+'''
