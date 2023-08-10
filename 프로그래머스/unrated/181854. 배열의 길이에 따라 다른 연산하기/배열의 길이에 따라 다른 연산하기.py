9753bbd65df804ef0f33a844cced0905d765427a
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-08-10 10:41:28 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -1,10 +1,16 @@
 def solution(arr, n):
     for idx, val in enumerate(arr):
-        if len(arr) % 2 == 0:   #짝수
+        if len(arr) % 2 == 0:           #짝수
             if idx % 2 == 1:
                 arr[idx] += n
         else:           #홀수
             if idx % 2 == 0:
                 arr[idx] += n
-    
     return arr
+
+'''
+* enumerate (파이썬 내장함수)
+- index와 원소 동시에 접근하면서 루프 돌리기 가능(index 변수 증가 없이)
+- for문의 in 부분을 enumerate()함수로 감싸기
+- 중첩 for문 작업 -> enumerate() 함수로 작업
+'''
