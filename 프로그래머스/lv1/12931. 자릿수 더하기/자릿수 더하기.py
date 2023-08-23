d87ec35745f86e3a19e0dbfad8e694ff99904b61
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-08-23 18:07:21 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -3,6 +3,7 @@ def solution(n):
     n = str(n)
     for i in n: #문자열 n을 for문 돌면서
         answer += int(i) #각 문자 i를 int정수로 변환해 더함
+        #숫자합을 구하기 위해 다시 정수형(int)로 변환한 것
     return answer
 
 '''# pythoninc code
