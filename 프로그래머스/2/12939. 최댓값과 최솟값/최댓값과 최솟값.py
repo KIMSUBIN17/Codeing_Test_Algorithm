8e91a5f49bf6bb903baad5a6732468e2bf194034
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-10-24 12:05:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,20 @@
 def solution(s):
     answer = ''
+    #s에는 둘 이상의 정수가 공백으로 구분되어 있음
+    #문자열로 나열된 숫자들은 int형으로 변환해주고 이를 list에 담음
     s = list(map(int,s.split()))
     return str(min(s)) + " " + str(max(s))
+
+
+'''
+다른풀이
+def solution(s):
+    answer = ''
+    # s에는 둘 이상의 정수가 공백으로 구분되어 있다
+    nums = s.split()
+    new_nums = []
+    for num in nums:
+        new_nums.append(int(num))
+    answer += '{0} {1}'.format(min(new_nums), max(new_nums))
+    return answer
+'''
