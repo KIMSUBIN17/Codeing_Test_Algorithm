eeb9f8b3d93a68f3ef00ad03befea230494e13e8
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-10-24 16:39:14 +0900

    Update 식품분류별 가장 비싼 식품의 정보 조회하기.sql

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -6,3 +6,12 @@ WHERE (CATEGORY,PRICE) IN (SELECT CATEGORY, MAX(PRICE)
                         GROUP BY CATEGORY
                         HAVING CATEGORY IN ('과자','국','김치', '식용유'))
 ORDER BY PRICE DESC
+
+
+
+
+/*
+서브쿼리에 2개이상의 컬럼이 쓰여지는 경우 작성법
+WHERE( COL1, COL2) IN (SELECT ~)
+https://everyday-deeplearning.tistory.com/entry/ORACLE-MULTIPLE-COLUMN-SUBQUERY%EC%99%80-EXIST
+*/
