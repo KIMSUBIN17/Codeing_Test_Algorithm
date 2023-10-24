7c95326ba5c7ac50a98d58e8111bb59ab9b3b223
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-10-24 11:40:25 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,18 +1,4 @@
 def solution(s):
     answer = ''
-    s = list(s.split())
-    #map함수 사용해 문자열의 각 부분을 정수로 변환
-    s_int = list(map(int,s))
-    min_s = min(s_int)
-    max_s = max(s_int)
-    
-    return f"{min_s} {max_s}"
-
-
-
-'''
-더 짧은 풀이
-def solution(s):
     s = list(map(int,s.split()))
     return str(min(s)) + " " + str(max(s))
-'''
