c42290da02f38e517c3baca47fe6b1f63f4889d9
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-11-02 15:58:17 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -0,0 +1,22 @@
+-- 코드를 입력하세요
+SELECT TO_CHAR(OS.SALES_DATE,'YYYY') AS YEAR
+,TO_NUMBER(TO_CHAR(OS.SALES_DATE,'MM')) AS MONTH
+, UI.GENDER
+, COUNT(DISTINCT OS.USER_ID) AS USERS
+FROM USER_INFO UI
+INNER JOIN ONLINE_SALE OS ON UI.USER_ID = OS.USER_ID
+GROUP BY TO_CHAR(OS.SALES_DATE,'YYYY') ,TO_NUMBER(TO_CHAR(OS.SALES_DATE,'MM')),UI.GENDER
+HAVING UI.GENDER IS NOT NULL
+ORDER BY YEAR, MONTH, GENDER ASC
+
+
+
+/* #오답노트
+1. GROUP BY 절을 작성하지 않았을때 
+ORA-00937: not a single-group group function 오류 발생
+그룹함수를 사용하였는데 GROUP BY 절을 사용하지 않아 발생한 에러로, 
+그룹함수를 사용하면 GROUP BY 절을 추가해야하며, GROUP BY 절에는 SELECT 절에 있는 모든 컬럼을 작성해야함
+2. 동일한 날짜, 회원 ID, 상품 ID 조합에 대해서는 하나의 판매 데이터만 존재
+--> USERS 회원수 COUNT할 때 DISTINCT로 중복제거 
+*/
+
