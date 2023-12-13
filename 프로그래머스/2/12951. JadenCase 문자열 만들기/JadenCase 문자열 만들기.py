b65695412313ab1c55dcd67096b9b71963c73083
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-12-13 16:22:35 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -14,3 +14,18 @@ split()은 공백이 몇개든 모든 공백을 하나로 처리하기때문에 
 split(' ') 사용하면 문자열 사이사이에 있는 공백만 분리 포인트로 삼을 수가 있음
 '''
 
+
+
+
+'''
+# 더 간단한 풀이 
+def solution(s):
+    words = s.split()  # 문자열을 공백을 기준으로 분리하여 리스트로 저장
+    answer = ' '.join([word.capitalize() for word in words])
+    return answer
+
+# 예시 테스트
+print(solution("3people unFollowed me"))  # "3people Unfollowed Me"
+print(solution("for the last week"))       # "For The Last Week"
+
+'''
