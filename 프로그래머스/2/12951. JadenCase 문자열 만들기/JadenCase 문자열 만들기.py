36ac1277a1d749be1f9e35a7ec3d3a9662beef81
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-12-13 16:21:35 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,14 +1,16 @@
 def solution(s):
     answer = ''
-    s = s.split(' ')
+    s = s.split(' ')    #문자열을 공백 기준으로 분리해 리스트로 저장
     for i in range(len(s)):
-        # capitalize() : 맨 첫글자만 대문자로 변환
         s[i] = s[i].capitalize()
     answer = ' '.join(s)
     return answer
+
+# capitalize() : 맨 첫글자만 대문자로 변환
     
 '''
 공백분리 시 그냥 split()만 하면 테스트케이스 몇개에서 오류 발생
 split()은 공백이 몇개든 모든 공백을 하나로 처리하기때문에 사용시 s는 알파벳,숫자, 공백문자(" ")로 이루어졌다는 조건에 걸림
 split(' ') 사용하면 문자열 사이사이에 있는 공백만 분리 포인트로 삼을 수가 있음
 '''
+
