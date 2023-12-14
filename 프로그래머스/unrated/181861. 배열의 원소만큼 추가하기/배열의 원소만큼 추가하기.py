f0ef86b9cc4ac9cc65e27524ee07d5fe22efee02
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-12-14 12:02:00 +0900

    Update 배열의 원소만큼 추가하기.py

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -4,3 +4,18 @@ def solution(arr):
     for num in arr:
         X.extend([num]* num)
     return X
+
+
+
+'''
+#오답노트 
+Q. append가 아니라 extend를 사용한 이유
+- append: 리스트에 하나의 원소를 추가할 때 사용
+ex) X.append(num) -> 리스트 X의 끝에 'num' 이라는 하나의 원소 추가 
+- extend: 리스트에 다른 리스트의 모든 원소를 추가할 때 사용
+ex) X.extend([num]*num) -> 리스트 X의 끝에 'num'이라는 원소를 'num'번 반복한 리스트를 추가
+
+위 문제는 각 원소의 값에 해당하는 숫자만큼 반복된 리스트를 추가해야하므로, 
+'extend'를 사용하여 한번에 여러 개의 원소를 추가함
+(append를 사용하면 각 반복마다 하나의 원소만 추가될 것)
+'''
