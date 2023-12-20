0c5c6ff7ff4093815725cb3cdf015005ae86ed93
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-12-20 16:54:53 +0900

    [level unrated] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
+++ 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
@@ -1,6 +1,7 @@
 def solution(strArr):
     answer = []
     
+    #인덱스 i와 해당 인덱스의 문자열 s를 for문으로 반복
     for i,s in enumerate (strArr):
         if i % 2 == 0 :
             answer.append(s.lower())
@@ -12,4 +13,8 @@ def solution(strArr):
 '''
 enumerate함수 사용해 인덱스와 원소를 동시에 받아옴
 최종적으로 문자열이 변환된 리스트를 리턴
+
+i : 인덱스 
+s : 해당 인덱스에 위치한 원소(문자열)
+enumerate를 사용하여 반복문을 돌 때 인덱스와 값을 함께 사용할 수 있음
 '''
