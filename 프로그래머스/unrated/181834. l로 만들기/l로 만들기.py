75062725c56cdbe8da579852da577fdf12410a9d
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2023-12-27 23:55:15 +0900

    [level unrated] Title: l로 만들기, Time: 6.06 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,18 @@
+def solution(myString):
+    result = ''
+    #문자열 myString을 한글자씩 순회하면서
+    for char in myString:
+        #l보다 앞서는 문자는 l로 바꾸고
+        if char < 'l':
+            result += 'l'
+        #아니면 그대로 둔다
+        else:
+            result += char
+    return result
+
+
+'''
+def solution(myString):
+    result = ''.join('l' if char < 'l' else char for char in myString)
+    return result
+'''
