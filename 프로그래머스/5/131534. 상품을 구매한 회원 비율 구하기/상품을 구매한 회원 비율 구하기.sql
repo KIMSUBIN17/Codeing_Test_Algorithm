4c0fbc6504e7c89e9deedef94b0e7fc6bf206b43
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2024-02-15 13:06:24 +0900

    Update 상품을 구매한 회원 비율 구하기.sql

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -7,3 +7,10 @@ FROM ONLINE_SALE
 WHERE USER_ID IN (SELECT USER_ID FROM USER_INFO WHERE TO_CHAR(JOINED, 'YYYY') = '2021')
 GROUP BY TO_CHAR(SALES_DATE,'YYYY'), TO_CHAR(SALES_DATE,'MM')
 ORDER BY YEAR ASC, MONTH ASC
+
+
+/*
+#오답노트
+- 비율구할때 같은 USER_ID가 여러 번 구매하는 경우도 있으므로 DISTICNT()를 사용해야 하는 것
+- 2021년에 가입한 회원 찾기 --> 2021년에 가입한 사람의 USER_ID를 조회해 WHERE절 서브쿼리에서 ONLINE_SALE 테이블의 USER_ID와 같은 ID를 조회함
+*/
