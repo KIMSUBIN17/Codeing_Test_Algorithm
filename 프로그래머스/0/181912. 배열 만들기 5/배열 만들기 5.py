5722c1e9801629f61ef7a41ca8b56d7280c3b146
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2024-03-19 15:42:11 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.70 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -3,11 +3,12 @@ def solution(intStrs, k, s, l):
     
     for string in intStrs:
         #시작 인덱스 s부터 l까지의 부분 문자열을 잘라내고
-        # 정수로 변환해서 k와 값을 비교하여, 변환한 정수가 k보다 크면 answer로 반환
+        #부분 문자열을 정수로 변환해서 
         substring = string[s:s+l]
+        
 
         num = int(substring)
-        
+       # k와 값을 비교하여, 변환한 정수가 k보다 크면 answer로 반환
         if num > k:
             answer.append(num)
             
