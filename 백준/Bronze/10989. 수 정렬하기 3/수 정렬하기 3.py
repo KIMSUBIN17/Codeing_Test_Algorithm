4c5c2145b5d6bb76bae69a0519623c37db85d79c
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2024-09-17 15:09:51 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8108 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(input())
+num_list = [0] * 10001
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    num_list[num] = num_list[num]+1
+   
+for i in range(10001):
+    if num_list[i] !=0:
+        for _ in range(num_list[i]):
+            print(i)
+    
