f8db1d8cec6b576c1bb21e5b5f86210fb536ad2f
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2024-09-17 15:11:22 +0900

    Update 약수 구하기.py

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,10 +1,13 @@
 N,K=map(int,input().split())
 arr=[] #약수들을 담은 배열
+
+#1~N까지반복하면서 N의 약수 찾기
 for i in range(1,N+1):
     if(N%i==0):
         arr.append(i)
 
 if(K<=len(arr)):
+    #리스트는  0부터 시작하므로 K-1번째 요소가 K번재
     print(arr[K-1])
 else:
     print(0)
