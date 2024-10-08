180984b6a22c6b468f72ac63f5666063e331572d
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2024-10-09 00:50:36 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -0,0 +1,16 @@
+N = int(input())  # N번 방 입력 받기
+
+# 1번 방은 예외 처리
+if N == 1:
+    print(1)
+else:
+    layer = 1  # 첫 번째 층 (1번 방)
+    count = 1  # 중앙에서부터 시작
+    
+    # 반복문을 통해 몇 번째 층에 있는지 찾음
+    # N번방이 포함될때까지 반복
+    while N > count:
+        count += 6 * layer  # 각 층에 추가되는 방의 수: 6 * layer
+        layer += 1
+
+    print(layer)
