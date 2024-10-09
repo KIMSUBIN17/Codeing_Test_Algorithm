ca4a45949cc8acca8a51fe2c1851eb67b1427874
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2024-10-09 21:46:26 +0900

    Update 웰컴 키트.py
    주석 추가

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -2,8 +2,13 @@ from math import ceil
 
 n = int(input())
 
-shorts = list(map(int, input().split()))
+shorts = list(map(int, input().split()))    #2 3 1 1 2 1을 입력하면, 리스트 shorts는 [2, 3, 1, 1, 2, 1]
+# 티셔츠의 묶음 크기 ㅅ, 펜의 묶음 크기 p를 입력
 t,p = map(int, input().split())
 
+#shorts의 각 요소 x에 대해 x/t를 올림처리하여 필요한 티셔츠 묶음수 계산하고, sum을 한다
 print(sum([ceil(x/t) for x in shorts]))
-print(n//p,n%p)
+
+# n // p : 참가자n을 펜 묶음  크기p로 나눈 몫 = 필요한 펜 묶음의 수
+# n % p  : 참가자 n을 펜 묶음 크기 p로 나눈 마머지,펜을 나누고 남은 참가자의 수
+print(n//p, n%p)
