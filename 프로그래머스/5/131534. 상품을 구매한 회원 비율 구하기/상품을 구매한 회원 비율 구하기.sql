3ceba0418e82bb3756475b556ac5bb3f008a5dad
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2024-10-17 17:02:50 +0900

    Update 상품을 구매한 회원 비율 구하기.sql
    오답이유 추가

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -15,3 +15,10 @@ ORDER BY YEAR ASC, MONTH ASC
 
 
 -- 신한카드 코딩테스트 유사문제 출제 
+/*
+#오답노트
+- 비율구할때 같은 USER_ID가 여러 번 구매하는 경우도 있으므로 DISTICNT()를 사용해야 하는 것(☆☆☆)
+- 2021년에 가입한 회원 찾기 --> 2021년에 가입한 사람의 USER_ID를 조회해 WHERE절 서브쿼리에서 ONLINE_SALE 테이블의 USER_ID와 같은 ID를 조회함
+
+
+*/
