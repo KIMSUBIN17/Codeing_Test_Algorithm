081f3929153099914f566bfc82c7aa49019c1fe7
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2025-01-14 12:22:58 +0900

    Update 스택 수열.py
    오답노트 및 로직 정리

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,9 +1,12 @@
+# 목표수열을 만들기 위해 수열의 크기 n을 입력받고, 
+# n만큼 반복해서 사용자가 입력하는 숫자를 정수로 변환해 find 리스트에 저장하는 리스트컴프리헨션
+# n번 반복에서 입력된 값을 정수로 변환하여 [] 로 변환된 값을 리스트에 담음
 n = int(input())  # 수열의 크기
 find = [int(input()) for _ in range(n)]  # 목표 수열 입력
 
-stack = []  # 스택 선언
-result = []  # 연산 기록
-current = 1  # 스택에 push할 수 시작 숫자
+stack = []  # 수를 담을 스택
+result = []  # 연산 기록을 저장할 리스트 선언 
+current = 1  # 현재 스택에 push할 숫자를 추적
 
 for target in find:
     # target에 도달할 때까지 push
@@ -23,3 +26,27 @@ for target in find:
 else:
     # 모든 연산 기록 출력
     print('\n'.join(result))
+
+
+
+
+
+'''
+💦 오답노트
+- 시간 복잡도 : O(n) 
+>> 각 숫자는 스택에 한번 push되고 한번 pop됨
+# n만큼 반복해서 사용자가 입력하는 숫자를 정수로 변환해 find 리스트에 저장하는 리스트컴프리헨션
+# n번 반복에서 입력된 값을 정수로 변환하여 [] 로 변환된 값을 리스트에 담음
+n = int(input())  # 수열의 크기
+find = [int(input()) for _ in range(n)]  # 목표 수열 입력
+>> 이러한 방식은 입력 갯수가 고정된 경우에 편리함
+sequence = []
+for _ in range(n):
+    sequence.append(int(input()))
+위 for문과 동일동작을 한다고 생각하면 됨 
+
+🧵 입력된 수열 처리
+- 입력된 수열을 순서대로 처리하며, 원하는 숫자(target)이 될 때까지 스택에 push
+- 스택의 top이 target과 일치하면 pop
+- 스택이 top이 target과 일치하지 않고, 더이상 push 불가하다면 수열을 만들 수 없으므로 NO 출력
+'''
